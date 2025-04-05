af61f55aa576eb3052971a2f47eafcfbb0ce1a6a
bt012314 <bt012314@ohio.edu>
2025-04-04 21:50:15 -0400

    Fix user registration and email verification logic

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from ufc_pickem import create_app
-from flask_migrate import upgrade
+#from flask_migrate import upgrade
 
 app = create_app()
 
 # Run migrations on startup
-with app.app_context():
-    upgrade()
+# with app.app_context():
+ #   upgrade()
